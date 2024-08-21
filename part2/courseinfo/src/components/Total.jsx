de58e9792d5597e72af332ae65dfb968c5f4f831
parthkaul-bit <127819564+parthkaul-bit@users.noreply.github.com>
2024-08-21 13:01:33 +0530

    exercise solution 2.2

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 
 const Total = (props) => {
+  let total = 0;
+  {
+    props.parts.map((part) => {
+      total += part.exercises;
+    });
+  }
   return (
-    <div>
-      <p>
-        Number of exercises{" "}
-        {props.parts[0].exercises +
-          props.parts[1].exercises +
-          props.parts[2].exercises}
-      </p>
-    </div>
+    <p>
+      <b>total of {total} exercises</b>
+    </p>
   );
 };
 

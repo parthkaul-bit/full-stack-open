d89c53f9e2ec14c40b25d13c3c5df4a19c80a339
parthkaul-bit <127819564+parthkaul-bit@users.noreply.github.com>
2024-08-22 08:55:01 +0530

    exercise solution 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,28 +4,37 @@ const App = () => {
   const [persons, setPersons] = useState([
     {
       name: "Arto Hellas",
+      number: 112,
     },
   ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState();
 
   const handleSubmit = (event) => {
     event.preventDefault();
     if (persons.map((person) => person.name).includes(newName)) {
       alert(`${newName} is already in the list`);
     } else {
-      setPersons([...persons, { name: newName }]);
+      setPersons([...persons, { name: newName, number: newNumber }]);
     }
     setNewName("");
+    setNewNumber("");
   };
-  const handleChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -33,7 +42,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => {
-        return <p key={person.name}>{person.name}</p>;
+        return (
+          <p key={person.name}>
+            {person.name} {person.number}
+          </p>
+        );
       })}
     </div>
   );

037d33d6c7cfc123c075593823360f45b3559535
parthkaul-bit <127819564+parthkaul-bit@users.noreply.github.com>
2024-08-27 16:04:51 +0530

    exercise solution 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -29,6 +29,17 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).send("Not Found");
+  }
+});
+
 app.get("/info", (request, response) => {
   let people = persons.length;
   const time = new Date();

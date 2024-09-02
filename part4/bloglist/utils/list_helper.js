565c56fc4795a16cd049b76526d8072a6b23cd98
parthkaul-bit <127819564+parthkaul-bit@users.noreply.github.com>
2024-09-02 19:32:45 +0530

    exercise solution 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,7 +10,19 @@ const totalLikes = (blogs) => {
   return totalLikes;
 };
 
+const favouriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+  const maxLikes = Math.max(...blogs.map((blog) => blog.likes));
+  const favouriteBlog = blogs.find((blog) => blog.likes === maxLikes);
+  return {
+    title: favouriteBlog.title,
+    author: favouriteBlog.author,
+    likes: favouriteBlog.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog,
 };

d0ca4866776d1d2189f77662895b2246bca536c0
parthkaul-bit <127819564+parthkaul-bit@users.noreply.github.com>
2024-09-09 19:29:27 +0530

    exercise solution 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,6 +6,7 @@ const blogsRouter = require("./controllers/blogs");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 logger.info("connecting to", config.MONGODB_URI);
 
@@ -23,5 +24,6 @@ app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 module.exports = app;

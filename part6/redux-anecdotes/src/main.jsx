64ba5f08555bd0540f496df6c6e4ff583a1c1465
parthkaul-bit <127819564+parthkaul-bit@users.noreply.github.com>
2024-09-25 14:33:25 +0530

    exercise solution 6.10

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,16 +1,24 @@
 import ReactDOM from "react-dom/client";
-import { createStore, combineReducers } from "redux";
+// import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import App from "./App";
 import filterReducer from "./reducers/filterReducer";
 import anecdoteReducer from "./reducers/anecdoteReducer";
+import { configureStore } from "@reduxjs/toolkit";
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer,
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterReducer,
+  },
 });
 
-const store = createStore(reducer);
+// const reducer = combineReducers({
+//   anecdotes: anecdoteReducer,
+//   filter: filterReducer,
+// });
+
+// const store = createStore(reducer);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>

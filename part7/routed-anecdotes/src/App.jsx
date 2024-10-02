fd61714a037d4938fbf39471e85e51a2356f42ea
parthkaul-bit <127819564+parthkaul-bit@users.noreply.github.com>
2024-10-02 19:37:47 +0530

    exercise 7.5

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, Routes, useMatch, useNavigate } from "react-router-dom";
 import { BrowserRouter, Route } from "react-router-dom";
+import { useField } from "./hooks";
 
 const Menu = () => {
   const padding = {
@@ -69,25 +70,26 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  // const [content, setContent] = useState("");
+  // const [author, setAuthor] = useState("");
+  // const [info, setInfo] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: props.content.value,
+      author: props.author.value,
+      info: props.info.value,
       votes: 0,
     });
     navigate("/");
-    props.setNotification(`a new anecdote ${content} created!`);
+    props.setNotification(`a new anecdote ${props.content.value} created!`);
 
     setTimeout(() => {
       props.setNotification("");
     }, 3000);
+    console.log(props);
   };
 
   return (
@@ -96,27 +98,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...props.content} name="content" />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...props.author} name="author" />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...props.info} name="info" />
         </div>
         <button>create</button>
       </form>
@@ -161,6 +151,9 @@ const App = () => {
   };
 
   const anecdoteById = (id) => anecdotes.find((a) => a.id === id);
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
 
   const vote = (id) => {
     const anecdote = anecdoteById(id);
@@ -189,7 +182,13 @@ const App = () => {
         <Route
           path="/create"
           element={
-            <CreateNew addNew={addNew} setNotification={setNotification} />
+            <CreateNew
+              addNew={addNew}
+              setNotification={setNotification}
+              content={content}
+              author={author}
+              info={info}
+            />
           }
         />
       </Routes>
